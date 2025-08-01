73a377139b65b50c0fd6ffdcb2d79e3252754c13
rafaelfernando70 <fernando.silva.rafael@escola.pr.gov.br>
2025-08-01 09:41:55 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+  
+    cartao.innerHTML = 
+    `<div class="cartao_conteudo">
+      <h3>${categoria}</h3>
+      <div class="cartao_conteudo_pergunta">
+        <p>${pergunta}</p>
+      </div>
+      <div class="cartao_conteudo_resposta">
+        <p>${resposta}</p>
+      </div>
+    </div>`
+  
+    container.appendChild(cartao)
+
+    let respostaEstaVisivel = false;
+function virarCartao() {
+  respostaEstaVisivel = !respostaEstaVisivel;
+  cartao.classList.toggle('active', respostaEstaVisivel);
+}
+
+cartao.addEventListener("click", virarCartao);
+container.appendChild(cartao);
+  }
---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é Javacript?',
+    'O Javascript é uma linguaguem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Paraná?',
+    'A capital é Curitiba'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python',
+    'O Python é uma linguagem de programação'
+)
